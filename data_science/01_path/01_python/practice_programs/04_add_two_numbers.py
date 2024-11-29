4146e7c75ccbb239bd6877d9a5297fa521a0df8f
alammehtab <mehtaba728@gmail.com>
2024-11-29 14:22:17 +0500

    add 04

---
+++ data_science/01_path/01_python/practice_programs/04_add_two_numbers.py
@@ -0,0 +1,6 @@
+num_one = float(input("Please enter the first number: "))
+num_two = float(input("Please enter the second number: "))
+
+sum = num_one + num_two
+
+print(f"The sum of {num_one} and {num_two} is {sum}.")
