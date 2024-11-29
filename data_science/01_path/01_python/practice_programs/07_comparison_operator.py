fa16757e9cc8a7f77097b0ca9ef29f016de41870
alammehtab <mehtaba728@gmail.com>
2024-11-29 14:38:25 +0500

    add 07

---
+++ data_science/01_path/01_python/practice_programs/07_comparison_operator.py
@@ -0,0 +1,11 @@
+# problem
+# Use comparison operator to find out whether a given variable ‘a’ is greater than ‘b’ or not. Take a = 34 and b = 80
+
+# solution
+a = 34
+b = 80
+
+if a > b:
+    print("Yes, A is greater than B.")
+else:
+    print("No, A is not greater than B.")
